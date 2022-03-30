aed7a9bfceb1d2c62ec767ce920cc5bd6ff7a4d0
sergio rodas <oscar.d.rodas@gmail.com>
2022-03-30 12:44:47 -0400

    segundo ejercicio

--- 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
+++ 01-Ruby/02-Flow-Array/02-Simple-looping/lib/simple_looping.rb
@@ -1,7 +1,25 @@
 def sum_with_for(min, max)
   # CONSTRAINT: you should use a for..end structure
+  return -1 if min > max
+
+  sum_correct = 0
+  if min < max
+    for num in (min..max)
+      sum_correct = num + sum_correct
+    end
+    return sum_correct
+  end
 end
 
 def sum_with_while(min, max)
   # CONSTRAINT: you should use a while..end structure
+  return -1 if min > max
+
+  i = min
+  sum = 0
+  while i <= max
+    sum += i
+    i += 1
+  end
+  return sum
 end
