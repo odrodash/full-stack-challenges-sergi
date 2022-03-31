176ae7eae0903ce2a39b7650df9190443cc32bfa
sergio rodas <oscar.d.rodas@gmail.com>
2022-03-30 22:31:00 -0400

    quinto commit de la clase

--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/croupier.rb
@@ -1,9 +1,18 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
-  # TODO: return (not print!) a message containing the player's score and bank's score
+  return "score is: #{player_score}, bank's score is: #{bank_score}."
 end
 
 def end_game_message(player_score, bank_score)
-  # TODO: return (not print!) a message telling if the user won or lost.
+  case
+  when player_score > 21 || player_score < bank_score
+    return "You lose"
+  when player_score == 21
+    return "Black Jack, You win"
+  when player_score > bank_score
+    return "You win"
+  when player_score == bank_score
+    return "You push"
+  end
 end
--- 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
+++ 01-Ruby/02-Flow-Array/04-Black-Jack/lib/interface.rb
@@ -1,4 +1,9 @@
 require_relative "croupier"
-
 # TODO: make the user play from terminal in a while loop that will stop
 #       when the user will not be asking for  a new card
+
+  puts "type 1 for choose a card or type yes for leave"
+  card = gets.chom
+
+  while card.downcase == "yes"
+    card.to_i =
