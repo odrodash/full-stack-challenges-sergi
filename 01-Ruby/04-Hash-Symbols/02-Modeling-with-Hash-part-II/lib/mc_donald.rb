81df0d0fc8e7b6f8b28607f73be918ce9ca36a39
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-01 16:09:48 -0400

    ejercicio resuelto hamburgeusa 2

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/mc_donald.rb
@@ -13,6 +13,20 @@ def poor_calories_counter(burger, side, beverage)
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
+COMIDA = {
+  "Happy Meal" => ["Cheese Burger", "French Fries", "Coca Cola"],
+  "Best Of Big Mac" => ["Big Mac", "French Fries", "Coca Cola"],
+  "Best Of McChicken" => ["McChicken", "Salad", "Sprite"]
+}
+
 def calories_counter(orders)
   # TODO: return number of calories for a less constrained order
+  sum_calories = orders.map do |mean|
+    if COMIDA.key?(mean)
+      poor_calories_counter(COMIDA[mean][0], COMIDA[mean][1], COMIDA[mean][2])
+    else
+      DISHES_CALORIES[mean]
+    end
+  end
+  return sum_calories.sum
 end
