5ad10ca54cf688124f998fa6acff420997d0fb63
sergio rodas <oscar.d.rodas@gmail.com>
2022-03-31 20:31:41 -0400

    splitter complete

--- 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Splitter/lib/splitter.rb
@@ -1,7 +1,12 @@
+b_array = []
+
 def size_splitter(array, size)
   # TODO: Return an array of two arrays, the first containing
   #       words of length `size`, the second containing all the other words
   #       In addition to this split, each array should be *sorted*.
+  first_sub_array = array.select {|word| word.length == size }.sort
+  second_sub_array = array.reject {|word| word.length == size }.sort
+  return [first_sub_array, second_sub_array]
 end
 
 def block_splitter(array)
@@ -9,4 +14,7 @@ def block_splitter(array)
   #       elements for which the given block yields true,
   #       the second containing all the other elements.
   #       No sort needed this time.
+  first_sub_array = array.select {|item| yield(item) }.sort
+  second_sub_array = array.reject {|item| yield(item) }.sort
+  return [first_sub_array, second_sub_array]
 end
