3948374f9cb95da321f9856b5e6efb208e96fb65
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-03 13:14:09 -0400

    segundo commit del array toi hash

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,8 +1,5 @@
 def array_to_hash(array)
   # TODO: implement the method :)
-  #definir metodo que tome array como parametro y devuelva un hash
-  #si no hay un bloque las key deberian ser los index de lo elemento en la matriz  convertidos en string
-  #else(si hay bloque) llamar pasandole el index de la array y usar lo que se devuelve como clave hash
   hash = Hash.new
   array.each_with_index do |elem, index|
     key = block_given? ? yield(index) : index.to_s
