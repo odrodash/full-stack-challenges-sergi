dc65d3bd807a397ce64f9830afdf0a3217a2473c
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-07 15:31:32 -0400

    restaurant complete rate

--- 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/04-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,22 @@
 class Restaurant
-  # TODO: add relevant accessors if necessary
+  attr_reader :average_rating, :city
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @rate = []
   end
 
-  # TODO: implement .filter_by_city and #rate methods
+  def rate(new_rate)
+    @rate << new_rate
+    @average_rating = @rate.sum.to_f / @rate.size
+  end
+
+  def self.filter_by_city(restaurants, city)
+    restaurants.select do |restaurant|
+      restaurant.city == city
+    end
+  end
+
 end
