47490a2e830cf60f4986b40b2406d7991e5e704c
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-12 11:44:40 -0400

    modificador arreglo del id

--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app/models/meal.rb
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app/models/meal.rb
@@ -2,8 +2,15 @@ class Meal
   attr_accessor :id
   attr_reader :name, :price
   def initialize(attributes = {})
+    @id = attributes[:id] || 1
     @name = attributes[:name]
     @price = attributes[:price]
-    @id = attributes[:id] || 1
   end
 end
+
+platos = Meal.new(name: "majadito", price: 10)
+p platos
+puts "------------"
+p platos.name
+p platos.price
+p platos.id
