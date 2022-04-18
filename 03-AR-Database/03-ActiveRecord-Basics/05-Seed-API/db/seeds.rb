53d21ff55999f27a3d0555daa8172e89b41c8cf1
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-18 15:40:27 -0400

    nose que hize ahorita vemos que pedo

--- 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/05-Seed-API/db/seeds.rb
@@ -2,6 +2,7 @@ require "json"
 require "rest-client"
 
 # TODO: Write a seed to insert 10 posts in the database fetched from the Hacker News API.
+
 read = RestClient.get "https://hacker-news.firebaseio.com/v0/topstories.json"
 posts = JSON.parse(read)
 
