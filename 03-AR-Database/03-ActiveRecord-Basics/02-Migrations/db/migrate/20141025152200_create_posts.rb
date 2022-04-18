5a6fc431434489df6abfeeece947e7cb89baa466
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-18 11:35:12 -0400

    active records migrations successful

--- 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/02-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |c|
+      c.string :title
+      c.string :url
+
+      c.timestamps
+    end
   end
 end
