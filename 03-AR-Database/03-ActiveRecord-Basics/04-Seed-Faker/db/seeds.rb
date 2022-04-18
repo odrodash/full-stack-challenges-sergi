2f9d7d7a64a91773a51d81c7c83eb5263cffe6ba
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-18 13:41:17 -0400

    seed faker completed and run

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-Faker/db/seeds.rb
@@ -1,2 +1,11 @@
 require "faker"
 # TODO: Write a seed to insert 100 posts in the database
+
+100.times do
+  posts = Post.new(
+    title: Faker::Company.name,
+    url: Faker::Cannabis.brand,
+    votes: rand(0..5)
+  )
+  posts.save
+end
