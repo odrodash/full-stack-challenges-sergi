437bcccc96545890104cd1ffc4c56f3b8b7211b3
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-19 15:21:19 -0400

    successful validations and the optional callback

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
 
   # TODO: Add some validation
   # TODO: Add some callbacks
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /.*@.*\.com/ }
+  before_validation :remove_whitespaces
+  after_create :send_welcome_email
+
+  private
+
+  def remove_whitespaces
+    self.email = email.strip unless email.nil?
+  end
+
+  def send_welcome_email
+    FakeMailer.instance.mail(email, "welcome!")
+  end
 end
