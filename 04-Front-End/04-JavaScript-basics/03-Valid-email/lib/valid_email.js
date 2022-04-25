13d29eeecf0ce62dd1ae61ea9e97aec8909f19bc
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-25 12:06:39 -0400

    completed email valid in js

--- 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-Front-End/04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,13 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  const emailValid = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  if (emailValid.test(email)) {
+    return true;
+  }
+  if (emailValid.test(email) === false) {
+    return false;
+  }
+  return true;
 };
 
 module.exports = valid; // Do not remove.
