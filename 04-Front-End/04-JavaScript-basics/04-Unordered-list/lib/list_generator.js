1025fc2209da89cc78611bc416b7a1187cefe264
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-25 13:46:53 -0400

    completed list unordered

--- 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-Front-End/04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,23 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  if (content === "") {
+    const tagItem2 = `<li class="list-group-item">${content}</li>`;
+    return tagItem2;
+  }
+  if (content) {
+    const tagItem = `<li class="list-group-item">${content}</li>`;
+    return tagItem;
+  }
+  return true;
 };
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  let resultado = "";
+  items.forEach((item) => {
+    resultado += `\n  ${listItem(item)}`;
+  });
+  return `<ul class="list-group">${resultado}\n</ul>`;
 };
 
 module.exports = { listItem, unorderedList }; // Do not remove.
