abe27c7a0e223406989408501f9254a7726310b3
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-26 16:19:41 -0400

    le wagon race completede

--- 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-DOM-and-Events/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,31 @@
 // TODO: write your code here
+const player1 = document.querySelectorAll('#player1-race td');
+const player2 = document.querySelectorAll('#player2-race td');
+const audioWinner = new Audio('../win.mp3');
+
+let race1 = 0;
+let race2 = 0;
+
+const pressKey = (k) => {
+  if (k.key === "q") {
+    race1 += 1;
+    player1[race1].classList.add('active');
+    player1[race1 - 1].classList.remove('active');
+  } else if (player1[race1].classList[0] === 'finish') {
+    audioWinner.play();
+    alert("Player 1 is Winner!");
+    window.location.reload();
+  }
+  if (k.key === "m") {
+    race2 += 1;
+    player2[race2].classList.add('active');
+    player2[race2 - 1].classList.remove('active');
+  } else if (player2[race2].classList[0] === 'finish') {
+    audioWinner.play();
+    alert("Player 2 is Winner!");
+    window.location.reload();
+  }
+  return true;
+};
+
+window.addEventListener('keyup', pressKey, false);
