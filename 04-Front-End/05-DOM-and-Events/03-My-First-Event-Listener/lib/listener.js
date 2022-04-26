0d42317edcaa12b7a9d80c387d9924fa9f489a27
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-26 14:31:36 -0400

    my first event listener completed

--- 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-DOM-and-Events/03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,9 @@
 // TODO: React to a click on the button!
+const audio = new Audio('../sound.mp3');
+
+const button = document.querySelector('#clickme');
+button.addEventListener('click', (Event) => {
+  button.classList = 'disabled';
+  button.innerText = 'Bingo!';
+  audio.play();
+});
