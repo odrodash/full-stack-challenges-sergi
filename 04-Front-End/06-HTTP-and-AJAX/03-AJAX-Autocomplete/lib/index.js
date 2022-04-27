691f217cabd2ae19d95af32c9273c8cd67fb1677
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-27 13:54:48 -0400

    ajax autocomplete successful

--- 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/03-AJAX-Autocomplete/lib/index.js
@@ -1,1 +1,16 @@
 // TODO: Autocomplete the input with AJAX calls.
+
+const input = document.querySelector('#search');
+const ul = document.querySelector('#results');
+
+input.addEventListener("keyup", (event) => {
+  ul.innerHTML = "";
+  fetch(`https://wagon-dictionary.herokuapp.com/autocomplete/${input.value}`)
+    .then(response => response.json())
+    .then((data) => {
+      data.words.forEach((word) => {
+        const li = `<li><a href="#" class="decoration">${word}</a></li>`;
+        ul.insertAdjacentHTML("beforeend", li);
+      });
+    });
+});
