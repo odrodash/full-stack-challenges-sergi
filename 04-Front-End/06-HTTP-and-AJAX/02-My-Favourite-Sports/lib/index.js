d6d3c06cd09d6340d076e98a6c13ad61aa1f2bfc
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-27 12:00:22 -0400

    completed my favorite sport

--- 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,12 @@
 // TODO: write your code here!
+const hoverActive = document.querySelectorAll('.clickable');
+
+const toggleActiveClass = ((item) => {
+  item.addEventListener('click', (event) => {
+    item.classList.toggle('active');
+  });
+});
+
+hoverActive.forEach((item) => {
+  toggleActiveClass(item);
+});
