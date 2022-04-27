3b120f1d1f7b63c06adc27d1ae2521fb50780e04
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-27 16:34:18 -0400

    anonymus chat completed full

--- 04-Front-End/06-HTTP-and-AJAX/04-Promo-Chat/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/04-Promo-Chat/lib/index.js
@@ -1,4 +1,43 @@
-const batch = 1; // change to your own batch id
-const baseUrl = "https://wagon-chat.herokuapp.com/";
+let batch = 1; // change to your own batch id
+const baseUrl = "https://wagon-chat.herokuapp.com/:channel/messages";
 
 // Your turn to code!
+
+const refresh = document.querySelector('#refresh');
+const li = document.querySelector('.list-unstyled');
+const h1 = document.querySelector('h1');
+let count = 0;
+
+const messagesGet = (item) => {
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then((data) => {
+      data.messages.forEach((message) => {
+        count += 1;
+        h1.innerText = `CHAT-ROOM (${batch += 1})`;
+        const insert = `<li>${message.content} (posted <span class="date">${count} minutes ago</span>) by ${message.author}</li>`;
+        item.insertAdjacentHTML('afterbegin', insert);
+      });
+    });
+};
+
+const messagePost = () => {
+  const message1 = document.getElementById("your-message").value;
+  const author1 = document.getElementById("your-name").value;
+  fetch(baseUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ author: author1, content: message1 })
+  }).then(response => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
+const form = document.querySelector("#comment-form");
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  messagePost();
+  messagesGet(li);
+});
