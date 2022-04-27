b90ede1c38ec23d6f101a9465fa05ff93044da8c
sergio rodas <oscar.d.rodas@gmail.com>
2022-04-27 11:23:16 -0400

    completed first exercise

--- 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
+++ 04-Front-End/06-HTTP-and-AJAX/01-Click-me/lib/index.js
@@ -1,8 +1,11 @@
 const displayAlertOnButtonClick = () => {
   // TODO: Select the big green button
-
+  const button = document.querySelector('button');
   // TODO: Bind the `click` event to the button
   // TODO: On click, display `Thank you!` in a JavaScript alert!
+  button.addEventListener('click', (event) => {
+    alert('Thank You!');
+  });
 };
 
 displayAlertOnButtonClick(); // Do not remove!
