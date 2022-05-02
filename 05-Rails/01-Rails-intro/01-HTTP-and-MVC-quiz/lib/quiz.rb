ef3d5908282a792c5ff0b073d4b0d4fd6d33fd1f
sergio rodas <oscar.d.rodas@gmail.com>
2022-05-02 11:29:31 -0400

    completed quiz the rails first exercise

--- 05-Rails/01-Rails-intro/01-HTTP-and-MVC-quiz/lib/quiz.rb
+++ 05-Rails/01-Rails-intro/01-HTTP-and-MVC-quiz/lib/quiz.rb
@@ -4,26 +4,22 @@
 def http_stands_for
   # TODO: Return a `string` with the full meaning of the acronym `HTTP`.
   # Hint, the format should look like this:  Hxxx-Txxx Txxx Pxxx
-  return ''
+  return 'Hyper-Text Transfer Protocol'
 end
 
 def http_requests_per_page
   # TODO: Return the variable that correctly describes the relationship between
   # a web page, and the number of HTTP requests required to load that
   # page.
-
-  a = 'There is always one HTTP request per page.'
   b = 'There is one HTTP request per resource/file needed for a web page (HTML, CSS, JS, etc).'
 
-  return  # Return the correct variable, `a` or `b`
+  return b # Return the correct variable, `a` or `b`
 end
 
 def view_responsibility
   # TODO: Return the variable whose value describes the responsibility of the
   # `View` in the MVC design pattern.
-  a = 'executes the appropriate reaction to user actions'
-  b = 'allows us to define data types in our application'
   c = 'displays information to users'
 
-  return  # Return the correct variable, `a`, `b` or `c`
+  return c # Return the correct variable, `a`, `b` or `c`
 end
