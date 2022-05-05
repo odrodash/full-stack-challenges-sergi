09c5726fab64919d527061eb0cd228a7bf559336
sergio rodas <oscar.d.rodas@gmail.com>
2022-05-05 11:20:46 -0400

    completed quiz front-end

--- 05-Rails/05-Rails-MC-with-images/01-Rails-frontend-quiz/lib/quiz.rb
+++ 05-Rails/05-Rails-MC-with-images/01-Rails-frontend-quiz/lib/quiz.rb
@@ -4,14 +4,17 @@
 
 def rails_app
   # TODO: Return a `String` with the right command to create a new rails app called `watch-list`.
+  return "rails new watch-list"
 end
 
 def modern_javascript
   # TODO: Return a `String` of what you have to add to `application.html.erb` if you want to import
   # the JavaScript file `application.js` that is inside of `app/javascript`
+  return "javascript_pack_tag 'application'"
 end
 
 def add_a_npm_package
   # TODO: Return a `String` with the command to type in the terminal to add the npm package `sweetalert`
   # to your rails app.
+  return "yarn add sweetalert"
 end
